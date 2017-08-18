76112b76bbfaceccf4f826ecc5122019ec981c9a
넙돌 <@DESKTOP-NDV5844>
2017-08-18 22:33:55 +0900

    메시지 추가

--- src/gugudan.java
+++ src/gugudan.java
@@ -3,8 +3,13 @@ public class gugudan {
 
 	public static void main(String[] args) {
 		
+		// 구구단 2단
+		
 		System.out.println("2");
 		System.out.println("4");
+		System.out.println("6");
+		System.out.println("8");
+		System.out.println("10");
 	
 	}
 	
