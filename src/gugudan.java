36400679df654820d42946f8146ebeebd13f64c4
넙돌 <@DESKTOP-NDV5844>
2017-08-19 11:40:22 +0900

    scanner로 입력 받기 > 변수 x에 입력값 넣기 > x 곱하기 1~9 출력

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,17 +1,71 @@
+import java.util.Scanner;
 
 public class gugudan {
 
 	public static void main(String[] args) {
 		
-		// 구구단 2단
 		
-		System.out.println("2");
-		System.out.println("4");
-		System.out.println("6");
-		System.out.println("8");
-		System.out.println("10");
-		System.out.println("12");
-	
+//		System.out.println("4단");
+//		
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+//		result = 4 * 10;
+//		System.out.println(result);
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		 Scanner scanner = new Scanner(System.in);
+	        int number = scanner.nextInt();
+	     System.out.println(number *1);
+	     System.out.println(number *2);
+	     System.out.println(number *3);
+	     System.out.println(number *4);
+	     System.out.println(number *5);
+	     System.out.println(number *6);
+	     System.out.println(number *7);
+	     System.out.println(number *8);
+	     System.out.println(number *9);
+	        
+		
+		
+//		// 구구단 2단
+//		
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+//	
+//		// 구구단 3단
+//		
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
 	}
 	
 }
