85032591fa31f0939a4e34b3987ac87ad9a31de2
넙돌 <@DESKTOP-NDV5844>
2017-08-19 12:07:50 +0900

    입력 받고, 입력값을 변수a, for로 변수b를 1~9로 지정, 출력 a*b

--- src/gugudan.java
+++ src/gugudan.java
@@ -4,6 +4,30 @@ public class gugudan {
 
 	public static void main(String[] args) {
 		
+		//6단 반복문 while
+//		int i = 1;
+//		while(i<10) {
+//		System.out.println(6 * i);
+//			i = i + 1; // i++
+//			
+//		}
+//		
+//		//7단 반복문 for
+//		for(int j = 1; j < 10; j++) {
+//		System.out.println(7 * j);
+//		}
+		
+		//입력 받아서, 변수로 지정하고, for로 반복시키기
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		 Scanner scanner = new Scanner(System.in);
+	        int number = scanner.nextInt();
+	        for(int i= 1; i<10; i++) {
+	 
+	        System.out.println(number * i);
+			
+		}
+		
 		
 //		System.out.println("4단");
 //		
@@ -28,19 +52,19 @@ public class gugudan {
 //		result = 4 * 10;
 //		System.out.println(result);
 		
-		System.out.println("구구단 중 출력할 단은? : ");
-		 Scanner scanner = new Scanner(System.in);
-	        int number = scanner.nextInt();
-	     System.out.println(number *1);
-	     System.out.println(number *2);
-	     System.out.println(number *3);
-	     System.out.println(number *4);
-	     System.out.println(number *5);
-	     System.out.println(number *6);
-	     System.out.println(number *7);
-	     System.out.println(number *8);
-	     System.out.println(number *9);
-	        
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		 Scanner scanner = new Scanner(System.in);
+//	        int number = scanner.nextInt();
+//	     System.out.println(number *1);
+//	     System.out.println(number *2);
+//	     System.out.println(number *3);
+//	     System.out.println(number *4);
+//	     System.out.println(number *5);
+//	     System.out.println(number *6);
+//	     System.out.println(number *7);
+//	     System.out.println(number *8);
+//	     System.out.println(number *9);
+//	        
 		
 		
 //		// 구구단 2단
